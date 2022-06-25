3ce2a3111235fbd45096f5c5359d9891f63c41c0
mbahbuy <mbahbagonku@gmail.com>
2022-06-25 10:25:47 +0700

    yuy

--- Register/program/Constant.php
+++ Register/program/Constant.php
@@ -5,4 +5,4 @@ define( "DB_USER", "root" );
 define( "DB_PASS", "" );
 define( "DB_NAME", "magang" );
 
-define( "BASEURL", "http://192.168.1.6/Tugas/proggram-magang/" );
+define( "BASEURL", "http://192.168.1.6:8080/Tugas/proggram-magang/Register/" );
