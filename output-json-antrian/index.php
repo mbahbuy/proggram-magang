8c2b2975035fc5bb7432b635e171eb1542a17609
mbahbuy <mbahbagonku@gmail.com>
2022-06-29 23:32:23 +0700

    bootstrap

---
+++ output-json-antrian/index.php
@@ -0,0 +1,93 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>output-json-antrian</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+</head>
+<body>
+<div id="notif"></div>
+
+<div class="modal fade" id="formTombol" tabindex="-1" aria-labelledby="formTombolLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="formTombolLabel">Pendaftaran Pasien</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" id="nik" placeholder="KTP/No Induk Kesehatan Nasional" autocomplete="off" required>
+        </div>
+
+        <div class="form-floating mt-3">
+            <select class="form-select" id="menuKesehatan">
+                <option value="01" selected>Rapit Test</option>
+                <option value="02">Vaksin</option>
+                <option value="03">Poli Umum</option>
+                <option value="04">Poli Gigi</option>
+                <option value="05">Poli Penyakit Kulit</option>
+                <option value="06">Poli Ibu dan Anak</option>
+                <option value="07">Poli Kangker</option>
+                <option value="08">Poli Psychology</option>
+            </select>
+            <label for="menuKesehatan">Training dimulai pada bulan:</label>
+        </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+        <button type="button" class="btn btn-primary" id="daftar">Daftar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script src="assets/js/bootstrap.js"></script>
+<script>
+    function alerts( alert='danger', text='' )
+    {
+        var notifPlace = document.getElementById('notif');
+        var text = document.createElement('div');
+        text.innerHTML = '<div class="alert alert-'+ alert +' alert-dismissible fade show" role="alert">'+ text +'<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+        text.classList.add('container');
+        notifPlace.appendChild(text);
+    };
+    function tombol()
+    {
+        var notifPlace = document.getElementById('notif');
+        var text = document.createElement('div');
+        text.innerHTML = '<div class="row row justify-content-md-center mt-5"><div class="col-md-auto mt-5"><button type="button" class="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#formTombol">Ambil Antrian</button></div></div>';
+        text.classList.add('container');
+        notifPlace.appendChild(text);
+    };
+    tombol();
+    document.getElementById('daftar').onclick = function()
+    {
+        // console.log( document.getElementById('nik').value );
+        var data = {
+            pasien_token : 'hjdhfjhsdihfisduhdfu',// diambil dari cookie/sessien
+            pasien_nik : document.getElementById('nik').value,
+            pasien_need : document.getElementById('menuKesehatan').value
+        };
+        var jsonData = JSON.stringify(data);
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if( this.readyState == 4 && this.status == 200 ){
+
+                // var dataphp = JSON.parse(this.responseText);
+                alert(this.responseText);
+
+            };
+        };
+        xhttp.open( "POST", "program/Database.php", true );
+        xhttp.setRequestHeader( "Content-Type", "application/json" );
+        xhttp.send( jsonData );
+    };
+</script>
+</body>
+</html>
---
+++ output-json-antrian/program/Database.php
@@ -0,0 +1,110 @@
+<?php require_once 'Constant.php'; $conn = new mysqli( DB_HOST, DB_USER, DB_PASS, DB_NAME );
+
+$datajson = file_get_contents( "php://input" );
+$data = json_decode( $datajson, true );
+$date = date( 'Y-m-d' );
+$textantrian = poli( $data['pasien_need'] ); 
+
+if( $data['pasien_token'] !== '' )
+{
+    if( $data['pasien_nik'] !== '' )
+    {
+        if( $data['pasien_need'] !== '' )
+        {
+            if( date( 'H:i:s' ) > "15:00:00" )
+            {
+                $alert['data'] = 0;
+                $alert['alert'] = 'danger';
+                $alert['text'] = 'Data tidak diterima.<br/>Pendaftaran Pasien telah habis.<br/>Daftar lagi besok!!!';
+                echo json_encode($alert); 
+            } elseif( date( 'H:i:s' ) < "04:00:00" )
+            {
+                $alert['data'] = 0;
+                $alert['alert'] = 'danger';
+                $alert['text'] = 'Data tidak diterima.<br/>Pendaftaran Pasien belum dibuka!!!';
+                echo json_encode($alert);
+            } else {
+                $sqlcheck = $conn->query( "SELECT * FROM book WHERE ( tanggal LIKE '%$date%' AND book_payment = $data[pasien_nik] AND produk_id = $data[pasien_need] ) ");
+                // echo "SELECT * FROM book WHERE ( tanggal LIKE '%$date%' AND book_payment = $data[pasien_nik] AND produk_id = $data[pasien_need] ) ";die;
+                if( $sqlcheck->num_rows > 0 )
+                {
+                    // echo 'ada';die; 
+                    $alert['data'] = 0;
+                    $alert['alert'] = 'danger';
+                    $alert['text'] = 'Maaf, Data tidak diterima.<br/>Anda telah mendapatkan nomor antrian hari ini.';
+                    echo json_encode($alert);
+                } else {
+                    // echo 'tidak ada';die;
+                    $dataAntrian = $conn->query( "SELECT book_timer FROM book WHERE ( tanggal LIKE '%$date%' AND produk_id = $data[pasien_need] ) ORDER BY book_timer DESC LIMIT 1 " );
+                    if( $dataAntrian->num_rows > 0 )
+                    {
+                        while( $p = $dataAntrian->fetch_assoc() )
+                        {
+                            $nomorAntrian = $p['book_timer'] + 1 ;
+    
+    
+                            $sql = "INSERT INTO book( user_token, produk_id, book_timer, book_payment )
+                                VALUES (
+                                    '$data[pasien_token]',
+                                    '$data[pasien_need]',
+                                    '$nomorAntrian',
+                                    '$data[pasien_nik]'
+                                )
+                            ";
+                            mysqli_query( $conn, $sql );
+                            $alert['data'] = $textantrian . " " . $nomorAntrian ;
+                            $alert['alert'] = 'success';
+                            $alert['text'] = 'Data diterima.';
+                            echo json_encode($alert);
+                        };
+    
+                    } else {
+                        $sql = "INSERT INTO book( user_token, produk_id, book_timer, book_payment )
+                            VALUES (
+                                '$data[pasien_token]',
+                                '$data[pasien_need]',
+                                '1',
+                                '$data[pasien_nik]'
+                            )
+                        ";
+                        mysqli_query( $conn, $sql );
+                        $alert['data'] = $textantrian . " 1" ;
+                        $alert['alert'] = 'success';
+                        $alert['text'] = 'Data diterima.';
+                        echo json_encode($alert);
+                    };
+    
+                };
+            };
+        } else {
+            $alert['data'] = 0;
+            $alert['alert'] = 'danger';
+            $alert['text'] = 'Maaf, Data tidak diterima.<br/>Lengkapi form pendaftaran terlebih dahulu.';
+            echo json_encode($alert);
+        }
+    } else {
+        $alert['data'] = 0;
+        $alert['alert'] = 'danger';
+        $alert['text'] = 'Maaf, Data tidak diterima.<br/>Lengkapi form pendaftaran terlebih dahulu.';
+        echo json_encode($alert);
+    }
+} else {
+    $alert['data'] = 0;
+    $alert['alert'] = 'danger';
+    $alert['text'] = 'Maaf, Data tidak diterima.<br/>Anda belum login.';
+    echo json_encode($alert);
+}
+function poli( $tuyul )
+{
+    $pemikat = [
+        "01" => "Rapit Test",
+        "02" => "Vaksin",
+        "03" => "Poli Umum",
+        "04" => "Poli Gigi",
+        "05" => "Poli Penyakit Kulit",
+        "06" => "Poli Ibu dan Anak",
+        "07" => "Poli Kangker",
+        "08" => "Poli Psychology",
+    ];
+    return $pemikat[ $tuyul ];
+}
